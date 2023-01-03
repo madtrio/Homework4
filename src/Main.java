739208154abcd84e7f22994d512fcf1144e02f48
madtro <anna.kozitzyna@yandex.ru>
2023-01-03 20:04:21 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Task 1");
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Если возраст человек равен " + age + ", то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человек равен " + age + ", то он не совершеннолетний, нужно немного подождать");
+        }
+
     }
 }
