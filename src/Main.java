7dc23d34f36c592913012ad0845a5661849121ad
madtro <anna.kozitzyna@yandex.ru>
2023-01-03 20:06:55 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,12 @@ public class Main {
         } else {
             System.out.println("Если возраст человек равен " + age + ", то он не совершеннолетний, нужно немного подождать");
         }
-
+        System.out.println("Task 2");
+        int temp = 4;
+        if (temp < 5) {
+            System.out.println("На улице холодно, нужно надеть шапку");
+        } else {
+            System.out.println("Сегодня тепло, можно идти без шапки");
+        }
     }
 }
